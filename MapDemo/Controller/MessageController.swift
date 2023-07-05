ca6e6cc5150366a1ab80f1d529b3037b8b797e1a
Allan John Valiente <allanjohn.valiente@gmail.com>
2023-07-05 01:25:53 -0400

    messaging option added clip and camera. with show and hide

--- MapDemo/Controller/MessageController.swift
+++ MapDemo/Controller/MessageController.swift
@@ -84,7 +84,7 @@ class MessageCollectionViewCell : UICollectionViewCell {
     }
 
 }
-class MessageController: UIViewController {
+class MessageController: UIViewController, UITextFieldDelegate {
     var cV: UICollectionView!
     var messages: [Message] = [
         Message(name: "Allan", messageInfo: "Thank you for ordering from Oh My Gulay!. We're currently preparing your order.", msgType: 1, dateTime: "01/30/2023 11:48 PM"),
@@ -188,62 +188,103 @@ class MessageController: UIViewController {
         t.layer.borderWidth = 1.0
         t.layer.borderColor = UIColor.gray.cgColor
         t.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: t.frame.height))
-        t.leftViewMode = .always
+        //t.leftViewMode = .always
         return t
+    }()
+    var activeOptionConstraints: [NSLayoutConstraint] = [NSLayoutConstraint]()
+    var unActiveOptionConstraints: [NSLayoutConstraint] = [NSLayoutConstraint]()
+    let imglc = UIImage(named:"lchevron")?.withRenderingMode(.alwaysTemplate)
+    let imgrc = UIImage(named:"rchevron")?.withRenderingMode(.alwaysTemplate)
+    var isHidden = false
+    lazy var imgShowHide: UIImageView = {
+        let imgSH = UIImageView(image: imglc!)
+        imgSH.tintColor = UIColor.init(hexString: "#CFCFCF")
+        imgSH.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(showOtherBtn)))
+        imgSH.isUserInteractionEnabled = true
+        return imgSH
+    }()
+    lazy var imgClip: UIImageView = {
+        let imgcl = UIImageView(image: UIImage(named:"clip")?.withRenderingMode(.alwaysTemplate))
+        imgcl.tintColor = UIColor.init(hexString: "#CFCFCF")
+        imgcl.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(attachedFile)))
+        imgcl.isUserInteractionEnabled = true
+        return imgcl
+    }()
+    
+    lazy var imgCamera: UIImageView = {
+        let imgca = UIImageView(image: UIImage(named:"camera")?.withRenderingMode(.alwaysTemplate))
+        imgca.tintColor = UIColor.init(hexString: "#CFCFCF")
+        imgca.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(attachedFile)))
+        imgca.isUserInteractionEnabled = true
+        return imgca
+    }()
+    lazy var innerView: UIView = {
+        let iv = UIView()
+        
+        iv.addSubview(imgShowHide)
+        imgShowHide.anchor(
+            top: iv.topAnchor, left: iv.leftAnchor,
+            bottom: iv.bottomAnchor, right: nil,
+            paddingTop: 17, paddingLeft: 0,
+            paddingBottom: 17, paddingRight: 10,
+            width: 30, height: 25)
+        
+        iv.addSubview(imgClip)
+        imgClip.anchor(
+            top: iv.topAnchor, left: imgShowHide.rightAnchor,
+            bottom: iv.bottomAnchor, right: nil,
+            paddingTop: 17, paddingLeft: 10,
+            paddingBottom: 17, paddingRight: 10,
+            width: 30, height: 25)
+        imgClip.isHidden = isHidden
+        
+        iv.addSubview(imgCamera)
+        imgCamera.anchor(
+            top: iv.topAnchor, left: imgClip.rightAnchor,
+            bottom: iv.bottomAnchor, right: nil,
+            paddingTop: 17, paddingLeft: 10,
+            paddingBottom: 17, paddingRight: 0,
+            width: 30, height: 25)
+        imgCamera.isHidden = isHidden
+        
+        return iv
     }()
     
     lazy var bottomNav: UIView = {
         let v  = UIView()
-        
         let imgs = UIImage(named:"send")?.withRenderingMode(.alwaysTemplate)
         var imgSend = UIImageView(image: imgs!)
         imgSend.tintColor = UIColor.init(hexString: "#CFCFCF")
         imgSend.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(send)))
         imgSend.isUserInteractionEnabled = true
-        
-        let imgcl = UIImage(named:"clip")?.withRenderingMode(.alwaysTemplate)
-        var imgClip = UIImageView(image: imgcl!)
-        imgClip.tintColor = UIColor.init(hexString: "#CFCFCF")
-        imgClip.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(attachedFile)))
-        imgClip.isUserInteractionEnabled = true
-        
-        let imgca = UIImage(named:"camera")?.withRenderingMode(.alwaysTemplate)
-        var imgCamera = UIImageView(image: imgca!)
-        imgCamera.tintColor = UIColor.init(hexString: "#C9C9C9")
-        imgCamera.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(addImage)))
-        imgCamera.isUserInteractionEnabled = true
-        
-        v.addSubview(imgClip)
-        imgClip.anchor(
+
+        v.addSubview(innerView)
+        innerView.anchor(
             top: v.topAnchor, left: v.leftAnchor,
             bottom: v.bottomAnchor, right: nil,
-            paddingTop: 17, paddingLeft: 20,
-            paddingBottom: 17, paddingRight: 15,
-            width: 30, height: 25)
-        
-        v.addSubview(imgCamera)
-        imgCamera.anchor(
-            top: v.topAnchor, left: imgClip.rightAnchor,
-            bottom: v.bottomAnchor, right: nil,
-            paddingTop: 17, paddingLeft: 15,
-            paddingBottom: 17, paddingRight: 15,
-            width: 30, height: 25)
+            paddingTop: 0, paddingLeft: 10,
+            paddingBottom: 0, paddingRight: 0,
+            width: 0, height: 25)
         
         v.addSubview(imgSend)
         imgSend.anchor(
             top: v.topAnchor, left: nil,
             bottom: v.bottomAnchor, right: v.rightAnchor,
             paddingTop: 17, paddingLeft: 15,
-            paddingBottom: 17, paddingRight: 20,
+            paddingBottom: 17, paddingRight: 15,
             width: 30, height: 25)
         
         v.addSubview(txtMessage)
         txtMessage.anchor(
-            top: v.topAnchor, left: imgCamera.rightAnchor,
+            top: v.topAnchor, left: innerView.rightAnchor,
             bottom: v.bottomAnchor, right: imgSend.leftAnchor,
-            paddingTop: 15, paddingLeft: 15,
+            paddingTop: 15, paddingLeft: 10,
             paddingBottom: 15, paddingRight: 15,
             width: 0, height: 40)
+        
+        activeOptionConstraints.append(innerView.widthAnchor.constraint(equalToConstant: 120))
+        unActiveOptionConstraints.append(innerView.widthAnchor.constraint(equalToConstant: 30))
+        showOtherBtn()
         return v
     }()
     func setUpView(){
@@ -284,6 +325,27 @@ class MessageController: UIViewController {
             paddingTop: 0, paddingLeft: 0,
             paddingBottom: 0, paddingRight: 0,
             width: 0, height: 0)
+    }
+
+    @objc private func showOtherBtn(){
+        UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.imgClip.isHidden = !self.isHidden
+            self.imgCamera.isHidden = !self.isHidden
+        })
+        imgShowHide.image = isHidden ? imgrc : imglc
+        
+        if isHidden {
+            NSLayoutConstraint.activate(activeOptionConstraints)
+            NSLayoutConstraint.deactivate(unActiveOptionConstraints)
+        }
+        else{
+            NSLayoutConstraint.activate(unActiveOptionConstraints)
+            NSLayoutConstraint.deactivate(activeOptionConstraints)
+        }
+            
+        innerView.updateConstraints()
+        innerView.layoutIfNeeded()
+        isHidden = !isHidden
     }
     
     @objc private func goBack(){
@@ -331,4 +393,5 @@ extension MessageController: UICollectionViewDelegate, UICollectionViewDataSourc
         let estimatedFrame = NSString(string: msg.messageInfo).boundingRect(with: size,options: .usesLineFragmentOrigin, context: nil)
         return CGSize(width: approximateWidth, height: estimatedFrame.height + 70)
     }
+     
 }
